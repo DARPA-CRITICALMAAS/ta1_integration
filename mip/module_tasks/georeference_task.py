c269fcc9166f17ad9c64d0ff80981ba34237f3ef
Michael P. Gerlek <mgerlek@inferlink.com>
2024-02-07 23:41:12 +0000

    add verification

--- mip/module_tasks/georeference_task.py
+++ mip/module_tasks/georeference_task.py
@@ -3,7 +3,7 @@
 from mip.utils.docker_task import DockerTask
 from mip.module_tasks.registry import register_task
 from mip.module_tasks.start_task import StartTask
-from mip.utils.checker import check_directory_exists
+from mip.utils.checker import check_file_exists
 
 
 @register_task
@@ -12,5 +12,5 @@ class GeoreferenceTask(DockerTask):
     REQUIRES = [StartTask]
 
     def run_post(self):
-        # TODO
         d = self.task_config.host_task_output_dir
+        check_file_exists(d / f"{self.map_name}.json", min_bytes=17)
