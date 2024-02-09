b84f41cb1045b98aeda6919cfbd3368884ff31d2
Michael P. Gerlek <mgerlek@inferlink.com>
2024-02-08 16:25:51 -0800

    first pass stack creator

---
+++ stack/template_parameters.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright 2024 InferLink Corporation
+
+from __future__ import annotations
+
+from pydantic import BaseModel
+from pydantic_yaml import to_yaml_str
+
+
+class TemplateParameters(BaseModel):
+    disk_gb: int
+    ec2_key_name: str
+    instance_type: str
+    owner_name: str
+    region: str
+    region_az: str
+    stack_name: str
+
+    def to_yaml(self) -> str:
+        return to_yaml_str(self)
