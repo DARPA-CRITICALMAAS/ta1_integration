0ac49d5dd791037fdd3ccc93575aba8391428fff
Michael P. Gerlek <mgerlek@inferlink.com>
2024-02-09 21:02:28 +0000

    build fix

--- docker/modules/text_spotting/build.sh
+++ docker/modules/text_spotting/build.sh
@@ -3,5 +3,5 @@ set -e
 
 ../../tools/dockerfile-include.py Dockerfile Dockerfile.tmp ../../tools
 
-docker build -t inferlink/ta1_text_spotting -f Dockerfile.tmp $REPO_DIR/usc-umn-inferlink-ta1/
+docker build -t inferlink/ta1_text_spotting -f Dockerfile.tmp $REPO_DIR
 
