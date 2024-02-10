bbc07be95c92ec24e6dd8d96933cf857359fbf8e
Michael P. Gerlek <mgerlek@inferlink.com>
2024-02-10 16:07:21 +0000

    updtaed dependences

--- mip/module_tasks/legend_item_description_task.py
+++ mip/module_tasks/legend_item_description_task.py
@@ -2,6 +2,7 @@
 
 from mip.module_tasks.registry import register_task
 from mip.utils.docker_task import DockerTask
+from mip.module_tasks.legend_segment_task import LegendSegmentTask
 from mip.module_tasks.legend_item_segment_task import LegendItemSegmentTask
 from mip.utils.checker import check_file_exists
 
@@ -9,7 +10,10 @@ from mip.utils.checker import check_file_exists
 @register_task
 class LegendItemDescriptionTask(DockerTask):
     NAME = "legend_item_description"
-    REQUIRES = [LegendItemSegmentTask]
+    REQUIRES = [
+        LegendSegmentTask,
+        LegendItemSegmentTask,
+    ]
 
     def run_pre(self):
         d = self.task_config.host_task_temp_dir
--- mip/module_tasks/line_extract_task.py
+++ mip/module_tasks/line_extract_task.py
@@ -2,13 +2,17 @@
 
 from mip.utils.docker_task import DockerTask
 from mip.module_tasks.legend_item_description_task import LegendItemDescriptionTask
+from mip.module_tasks.map_crop_task import MapCropTask
 from mip.module_tasks.registry import register_task
 
 
 @register_task
 class LineExtractTask(DockerTask):
     NAME = "line_extract"
-    REQUIRES = [LegendItemDescriptionTask]
+    REQUIRES = [
+        LegendItemDescriptionTask,
+        MapCropTask,
+    ]
 
     def run_post(self):
         # TODO
