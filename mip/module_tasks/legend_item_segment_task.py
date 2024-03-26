e223f88bc940f21a62ac88352d3f75d3deadd91e
Michael P. Gerlek <mgerlek@inferlink.com>
2024-03-26 16:37:25 +0000

    remove OBE code

--- mip/module_tasks/legend_item_segment_task.py
+++ mip/module_tasks/legend_item_segment_task.py
@@ -20,12 +20,3 @@ class LegendItemSegmentTask(DockerTask):
     def run_post(self):
         d = self.task_config.host_task_output_dir
         check_file_exists(d / f"{self.map_name}_PointLineType.geojson")
-
-        # TODO: remove this fixup step
-        src = (self.task_config.host_input_dir
-               / "maps"
-               / self.config.map_name
-               / "legend_item_segment"
-               / f"{self.config.map_name}.json")
-        dst = self.task_config.host_output_dir / self.task_config.task_name / "output"
-        shutil.copyfile(src, dst)
