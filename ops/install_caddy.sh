865c8b7800cf1d25d27d14e5f4dc33ac8c70ae64
Michael P. Gerlek <mgerlek@inferlink.com>
2024-04-01 21:49:05 -0700

    missing line?

--- ops/install_caddy.sh
+++ ops/install_caddy.sh
@@ -4,6 +4,7 @@ set -e
 
 # This script must be run using sudo
 
+apt-get update -y
 apt-get install -y debian-keyring debian-archive-keyring apt-transport-https curl
 rm -f /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' \
